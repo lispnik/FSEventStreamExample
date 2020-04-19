fe281cfaafbc5cb5b0065f1d8e3bc20081179372
Matthew Kennedy <burnsidemk@gmail.com>
2020-04-18 19:44:12 -0500

    Initial Commit

---
+++ Example2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Example2
+//
+//  Created by Matthew Kennedy on 4/18/20.
+//  Copyright © 2020 Matthew Kennedy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
